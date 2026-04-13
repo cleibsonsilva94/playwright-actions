dd89b4d1472518cc6a9e961d7284973dfc1b22cb
Lima <cleibsonslima@gmail.com>
2026-04-13 16:53:32 -0300

    Pre-correction of the 'Required User' scenari and addition of the base URL in the configuration file

--- Commands/Comandos.ts
+++ Commands/Comandos.ts
@@ -5,3 +5,7 @@
 // Para iniciar o projeto do zero:
 // npm init playwright@latest
 //
+
+//Iniciar testes com a interface gráfica do navegador visível:
+// npmx playwright test --headed
+//
--- playwright.config.ts
+++ playwright.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
+    baseURL: 'http://loginxp.vercel.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -39,35 +40,35 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
     // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
     // },
 
-    /* Test against branded browsers. */
     // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
     // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+
+    // /* Test against mobile viewports. */
+    // // {
+    // //   name: 'Mobile Chrome',
+    // //   use: { ...devices['Pixel 5'] },
+    // // },
+    // // {
+    // //   name: 'Mobile Safari',
+    // //   use: { ...devices['iPhone 12'] },
+    // // },
+
+    // /* Test against branded browsers. */
+    // // {
+    // //   name: 'Microsoft Edge',
+    // //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // // },
+    // // {
+    // //   name: 'Google Chrome',
+    // //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // // },
   ],
 
   /* Run your local dev server before starting the tests */
